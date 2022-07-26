a066f5013cb37c10f7d8850af920e159843bb84f
mymaks <100081290+Mymaks@users.noreply.github.com>
2022-07-27 01:21:49 +0300

    Update main.tf

--- preview-env/main.tf
+++ preview-env/main.tf
@@ -8,7 +8,7 @@ data "terraform_remote_state" "shared" {
   config = {
     organization = "Mymaks"
     workspaces = {
-      name = "hcup-be-shared"
+      name = "hcup-be-update-frontend"
     }
   }
 }
